4068adac1e0e020e6cf789b2b8e200de4c5a8311
Victor Vale <joaovictorvale06@gmail.com>
2025-08-08 18:50:09 -0300

    aula 18 upload e download de arquivos

--- rest-with-spring-boot-and-java/src/main/java/br/com/jvictorvale/exception/handler/CustomEntityResponseHandler.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/jvictorvale/exception/handler/CustomEntityResponseHandler.java
@@ -1,8 +1,6 @@
 package br.com.jvictorvale.exception.handler;
 
-import br.com.jvictorvale.exception.ExceptionResponse;
-import br.com.jvictorvale.exception.RequiredObjectIsNullException;
-import br.com.jvictorvale.exception.ResourceNotFoundException;
+import br.com.jvictorvale.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -48,5 +46,27 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
         );
 
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleFileNotFoundExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(FileStorageException.class)
+    public final ResponseEntity<ExceptionResponse> handleFileStorageExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
